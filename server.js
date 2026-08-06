e524d757d836c164fe421966eb873e6f4216e955
‪Dvir Getter‬‏ <dvirgetter13@gmail.com>
2026-08-06 22:52:42 +0300

    Fix formatting issue in server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-﻿const express = require('express');
+const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const bcrypt = require('bcryptjs');
